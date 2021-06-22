d488bdda38bac3ccf207fd3efb47a12fa85d1340
busbate <busbater@protonmail.com>
2021-06-22 22:18:44 +0800

    format

--- help.sh
+++ help.sh
@@ -1,45 +1,46 @@
 dir_code=$dir_log/code
 
 default_env_name=(
-  JD_COOKIE
-  FRUITSHARECODES
-  PETSHARECODES
-  PLANT_BEAN_SHARECODES
-  DREAM_FACTORY_SHARE_CODES
-  DDFACTORY_SHARECODES
-  JDZZ_SHARECODES
-  JDJOY_SHARECODES
-  JXNC_SHARECODES
-  BOOKSHOP_SHARECODES
-  JD_CASH_SHARECODES
-  JDSGMH_SHARECODES
-  JDCFD_SHARECODES
-  JDHEALTH_SHARECODES
+    JD_COOKIE
+    FRUITSHARECODES
+    PETSHARECODES
+    PLANT_BEAN_SHARECODES
+    DREAM_FACTORY_SHARE_CODES
+    DDFACTORY_SHARECODES
+    JDZZ_SHARECODES
+    JDJOY_SHARECODES
+    JXNC_SHARECODES
+    BOOKSHOP_SHARECODES
+    JD_CASH_SHARECODES
+    JDSGMH_SHARECODES
+    JDCFD_SHARECODES
+    JDHEALTH_SHARECODES
 )
 default_var_name=(
-  Cookie
-  ForOtherFruit
-  ForOtherPet
-  ForOtherBean
-  ForOtherDreamFactory
-  ForOtherJdFactory
-  ForOtherJdzz
-  ForOtherJoy
-  ForOtherJxnc
-  ForOtherBookShop
-  ForOtherCash
-  ForOtherSgmh
-  ForOtherCfd
-  ForOtherHealth
+    Cookie
+    ForOtherFruit
+    ForOtherPet
+    ForOtherBean
+    ForOtherDreamFactory
+    ForOtherJdFactory
+    ForOtherJdzz
+    ForOtherJoy
+    ForOtherJxnc
+    ForOtherBookShop
+    ForOtherCash
+    ForOtherSgmh
+    ForOtherCfd
+    ForOtherHealth
 )
-import_help_config () {
+
+import_help_config() {
     [ ! -n "$env_name" ] && env_name=($(echo ${default_env_name[*]}))
     [ ! -n "$var_name" ] && var_name=($(echo ${default_var_name[*]}))
 }
 
 ## 统计用户数量
-count_user_sum () {
-    for ((i=1; i<=${SUM:-$((3 * 4))}; i++)); do
+count_user_sum() {
+    for ((i = 1; i <= ${SUM:-$((3 * 4))}; i++)); do
         local tmp1=Cookie$i
         local tmp2=${!tmp1}
         [[ $tmp2 ]] && user_sum=$i || break
@@ -63,7 +64,7 @@ combine_sub() {
 }
 
 ## 正常依次运行时，组合所有账号的Cookie与互助码
-combine_all() {       
+combine_all() {
     echo "组合所有账号的Cookie与互助码,共"${#env_name[*]}"个"
     for ((i = 0; i < ${#env_name[*]}; i++)); do
         result=$(combine_sub ${var_name[i]})
@@ -74,11 +75,11 @@ combine_all() {
 }
 
 if [[ $p1 == *.js ]]; then
-     if [[ $AutoHelpOther == true ]] && [[ $(ls $dir_code) ]]; then
+    if [[ $AutoHelpOther == true ]] && [[ $(ls $dir_code) ]]; then
         echo "导入code生成的互助码"
         local latest_log=$(ls -r $dir_code | head -1)
         . $dir_code/$latest_log
-    fi       
+    fi
 fi
 import_help_config
 count_user_sum
--- sample/task_before..sh
+++ sample/task_before..sh
@@ -1,54 +1,30 @@
-## 说明
-
-青龙 2.8.0 更新后功能添加：  
-1、tempBlockCookie 功能  
-2、内部互助功能
-
-### 注意
-
-环境变量的 jd cookie 名称分别依次命名为 Cookie1、Cookie2  
-原定时互助码 ql code 更改为 bash /ql/repo/vipGhost0_ql_help/code.sh
-
-#### task_before.sh
-
-文件名精简为简称，用来作用 TempBlockCookie 参数
-
-```bash
 if [[ $1 =~ \. ]]; then
- echo 文件简称为：$(echo ${1%%.*}|sed -r "s/.*(j[d|x]_.*)/\1/g")（作用为屏蔽某个ck）
+  echo 文件简称为：$(echo ${1%%.*} | sed -r "s/.*(j[d|x]_.*)/\1/g")（作用为屏蔽某个ck）
 fi
-case $(echo ${1%%.*}|sed -r "s/.*(j[d|x]_.*)/\1/g") in
-    jd_pigPet | jd_daily_egg | jd_dreamFactory  )
-     TempBlockCookie="2 4 5"      # 账号5不玩东东农场
-     ;;
-   jd_jdfactory | jd_jxnc | jd_jxmc | jd_car_exchange | jd_jin_tie | jd_cfd )
-     TempBlockCookie="1 2 3 4 5"      # 账号2不玩京喜工厂和东东工厂
-     ;;
-   jd_health | jd_health_collect | jd_jump | jd_mcxhd )
-    TempBlockCookie="5"    # 新号火爆
-     ;;
-   jd_bean_home | jd_bean_sign | jd_blueCoin | jd_car | jd_cash | jd_club_lottery | jd_jdzz | jd_lotteryMachine | jd_redPacket | jd_superMarket | jd_beauty | jd_family | jd_sgmh | jd_speed_redpocke | jd_speed_sign | jd_syj | jd_carnivalcity | jd_xtg | jd_xtg_help | jd_gold_creator | jd_mohe )
-    TempBlockCookie="2 5"    # 火爆
-     ;;
-   jd_getFollowGift | jd_EsportsManager)
-    TempBlockCookie="2 5"    # 火爆
-     ;;
-   jd_ms )
-    TempBlockCookie="2 3 4 5" #  金融限定
-     ;;
- esac
+case $(echo ${1%%.*} | sed -r "s/.*(j[d|x]_.*)/\1/g") in
+jd_pigPet | jd_daily_egg | jd_dreamFactory)
+  TempBlockCookie="2 4 5" # 账号5不玩东东农场
+  ;;
+jd_jdfactory | jd_jxnc | jd_jxmc | jd_car_exchange | jd_jin_tie | jd_cfd)
+  TempBlockCookie="1 2 3 4 5" # 账号2不玩京喜工厂和东东工厂
+  ;;
+jd_health | jd_health_collect | jd_jump | jd_mcxhd)
+  TempBlockCookie="5" # 新号火爆
+  ;;
+jd_bean_home | jd_bean_sign | jd_blueCoin | jd_car | jd_cash | jd_club_lottery | jd_jdzz | jd_lotteryMachine | jd_redPacket | jd_superMarket | jd_beauty | jd_family | jd_sgmh | jd_speed_redpocke | jd_speed_sign | jd_syj | jd_carnivalcity | jd_xtg | jd_xtg_help | jd_gold_creator | jd_mohe)
+  TempBlockCookie="2 5" # 火爆
+  ;;
+jd_getFollowGift | jd_EsportsManager)
+  TempBlockCookie="2 5" # 火爆
+  ;;
+jd_ms)
+  TempBlockCookie="2 3 4 5" #  金融限定
+  ;;
+esac
+
 ##调用助力
-. /ql/repo/vipGhost0_ql_help/help.sh
-```
+. /ql/repo/busbate_ql_extra/help.sh
 
-#### 使用环境变量
-
-使用原 config.sh 的参数,互助设置已添加为内置参数，可不添加  
-添加 name*js_prefix 参数批量前缀  
-库为 JDHelloWorld 可设置为 JDHelloWorld_jd_scripts*  
-库为 chinnkarahoi 可设置为 chinnkarahoi*jd_scripts*
-
-```bash
 ## 自动按顺序进行账号间互助（选填） 设置为 true 时，将直接导入code最新日志来进行互助
 AutoHelpOther="true"
 
@@ -61,11 +37,7 @@ HelpType=""
 
 ##name_js_prefix 批量前缀，可设置为JDHelloWorld_jd_scripts_
 name_js_prefix="JDHelloWorld_jd_scripts_"
-```
 
-以下为互助设置，已添加为内置参数，可不添加
-
-```bash
 ## 需组合的环境变量列表，env_name需要和var_name一一对应，如何有新活动按照格式添加(不懂勿动)
 env_name=(
   JD_COOKIE
@@ -104,7 +76,6 @@ var_name=(
 ## name_js为脚本文件名，如果使用ql repo命令拉取，文件名含有作者名
 ## 所有有互助码的活动，把脚本名称列在 name_js 中，对应 config.sh 中互助码后缀列在 name_config 中，中文名称列在 name_chinese 中。
 ## name_js、name_config 和 name_chinese 中的三个名称必须一一对应。
-
 name_js=(
   jd_fruit
   jd_pet
@@ -120,6 +91,7 @@ name_js=(
   jd_cfd
   jd_health
 )
+
 name_config=(
   Fruit
   Pet
@@ -135,6 +107,7 @@ name_config=(
   Cfd
   Health
 )
+
 name_chinese=(
   东东农场
   东东萌宠
@@ -150,4 +123,3 @@ name_chinese=(
   京喜财富岛
   东东健康社区
 )
-```
